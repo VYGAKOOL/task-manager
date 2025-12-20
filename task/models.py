bbdd91bae72ce467e301f6cdb93fa306f07a876b
VYGAKOOL <vygakool@gmail.com>
2025-12-20 20:07:57 +0200

    fix flake8 in models.py

--- task/models.py
+++ task/models.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class Position(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class TaskType(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -19,8 +21,8 @@ class TaskType(models.Model):
 class Worker(AbstractUser):
     position = models.ForeignKey(
         Position,
-        on_delete= models.PROTECT,
-        related_name= "workers",
+        on_delete=models.PROTECT,
+        related_name="workers",
     )
 
     def __str__(self):
