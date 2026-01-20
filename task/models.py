ff3160525be103422e4c29b70ab92305867ca06e
VYGAKOOL <vygakool@gmail.com>
2026-01-20 05:45:26 +0200

    Rebase some models

--- task/models.py
+++ task/models.py
@@ -1,14 +1,6 @@
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-
-class Position(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
 
 
 class TaskType(models.Model):
@@ -16,17 +8,6 @@ class TaskType(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Worker(AbstractUser):
-    position = models.ForeignKey(
-        Position,
-        on_delete=models.PROTECT,
-        related_name="workers",
-    )
-
-    def __str__(self):
-        return f"{self.username} ({self.position})"
 
 
 class Task(models.Model):
