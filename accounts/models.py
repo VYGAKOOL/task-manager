7b1df488d37871f55d1f14952974d50b36e3d373
VYGAKOOL <vygakool@gmail.com>
2026-01-20 05:52:55 +0200

    Rebase models

---
+++ accounts/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.PROTECT,
+        related_name="workers",
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.position})"
