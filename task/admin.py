97455cf23d609db3567094ca931fea9ea6ebb39c
VYGAKOOL <vygakool@gmail.com>
2026-01-20 05:43:08 +0200

    Refactor admin.py

--- task/admin.py
+++ task/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Position, TaskType, Worker, Task
-
-
-@admin.register(Position)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-    search_fields = ("name",)
+from task.models import TaskType, Task
 
 
 @admin.register(TaskType)
@@ -15,25 +8,15 @@ class TaskTypeAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
-@admin.register(Worker)
-class WorkerAdmin(UserAdmin):
-    list_display = (
-        "username",
-        "email",
-        "first_name",
-        "last_name",
-        "position",
-        "is_staff",
-        "is_active",
-    )
-    list_filter = ("position", "is_staff", "is_active")
-    search_fields = ("username", "email", "first_name", "last_name")
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("position",)}),)
-
-
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ("name", "task_type", "priority", "is_completed", "deadline")
+    list_display = (
+        "name",
+        "task_type",
+        "priority",
+        "is_completed",
+        "deadline"
+    )
     list_filter = ("priority", "task_type", "is_completed")
     search_fields = ("name", "description")
     filter_horizontal = ("assignees",)
