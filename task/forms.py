2bec558df625f6705c3aa31e44e48cad450766f6
VYGAKOOL <vygakool@gmail.com>
2026-01-20 05:44:30 +0200

    Rebase forms

--- task/forms.py
+++ task/forms.py
@@ -1,29 +1,16 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
-from task.models import Position, Worker, Task
+from task.models import Task
 
 User = get_user_model()
 
 
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    position = forms.ModelChoiceField(queryset=Position.objects.all(), required=True)
-
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = ("username", "email", "password1", "password2", "position")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for fieldname in ["username", "password1", "password2"]:
-            self.fields[fieldname].help_text = None
-
-
 class TaskForm(forms.ModelForm):
     assignees = forms.ModelMultipleChoiceField(
-        queryset=User.objects.all(), widget=forms.CheckboxSelectMultiple, required=False
+        queryset=User.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
     )
 
     class Meta:
