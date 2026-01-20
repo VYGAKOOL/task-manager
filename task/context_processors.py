8f729c505ade2a0b370c5189e6eff485f9378dad
VYGAKOOL <vygakool@gmail.com>
2026-01-20 05:43:57 +0200

    flake8 fix

--- task/context_processors.py
+++ task/context_processors.py
@@ -1,4 +1,6 @@
 def navbar_visibility(request):
     match = request.resolver_match
     no_sidebar_pages = ["welcome", "login", "sign-up"]
-    return {"show_sidebar": not match or match.url_name not in no_sidebar_pages}
+    return {
+        "show_sidebar": not match or match.url_name not in no_sidebar_pages
+    }
